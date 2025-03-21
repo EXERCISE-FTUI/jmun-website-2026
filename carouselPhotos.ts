2eeb81343942d682359cf6cf53dfa4b03d7c40ee
Fatih20 <13521060@std.stei.itb.ac.id>
2025-03-21 22:35:14 +0700

    chore: compress images

--- carouselPhotos.ts
+++ carouselPhotos.ts
@@ -1,12 +1,12 @@
-import firstPhoto from "./public/carousel-photos/DSCF0890.jpg";
-import secondPhoto from "./public/carousel-photos/ADS_0008.jpg";
-import thirdPhoto from "./public/carousel-photos/ADS_0024.jpg";
-import fourthPhoto from "./public/carousel-photos/DSC08049.jpg";
-import fifthPhoto from "./public/carousel-photos/DSC08056.jpg";
-import sixthPhoto from "./public/carousel-photos/DSC08756.jpg";
-import seventhPhoto from "./public/carousel-photos/DSC08830.jpg";
-import eightPhoto from "./public/carousel-photos/DSCF0133.jpg";
-import ninthPhoto from "./public/carousel-photos/DSCF6718.jpg";
+import firstPhoto from "./public/carousel-photos/DSCF0890.jpeg";
+import secondPhoto from "./public/carousel-photos/ADS_0008.jpeg";
+import thirdPhoto from "./public/carousel-photos/ADS_0024.jpeg";
+import fourthPhoto from "./public/carousel-photos/DSC08049.jpeg";
+import fifthPhoto from "./public/carousel-photos/DSC08056.jpeg";
+import sixthPhoto from "./public/carousel-photos/DSC08756.jpeg";
+import seventhPhoto from "./public/carousel-photos/DSC08830.jpeg";
+import eightPhoto from "./public/carousel-photos/DSCF0133.jpeg";
+import ninthPhoto from "./public/carousel-photos/DSCF6718.jpeg";
 
 export const carouselPhotos = [
   firstPhoto,
